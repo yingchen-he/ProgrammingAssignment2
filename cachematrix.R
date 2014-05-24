933712b98da64f8178507098a310fc3df58b0fde
springtheo <spring.theo@gmail.com>
2014-05-23 21:11:40 -0500

    Submission for coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# The first function, makeCasheMatrix, makes a special matrix where the inverse of this matrix could be stored.
+# The second function, casheSolve, returns the inverse of a matrix.
+
+
+
+# This function creates a list that contains a list of functions that allows you to: 
+      # a) set the value of a matrix (x$set()), 
+      # b) get the value of the matrix (x$get()), 
+      # c) set the value of the inverse of this matrix (x$setinv()), and
+      # d) get the value of this inverse matrix (x$getinv()).
+# When creating a matrix using this function, its inverse matrix will be solved and saved. But it could also be set with the x$setinv() function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      xinv <- NULL
+      set <- function(y) {
+            x <<- y
+            xinv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inv) xinv <<- inv
+      getinv <- function() xinv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+# This function returns the inverse of a matrix. If the inverse has already been cached, it gets that matrix. If not, it solves the inverse and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      xinv <- x$getinv()
+      if(!is.null(xinv)) {
+            message("getting cached data")
+            return(xinv)
+      }
+      data <- x$get()
+      xinv <- solve(data, ...)
+      x$setinv(xinv)
+      xinv
 }
